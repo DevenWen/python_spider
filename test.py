b043777909dae0c450d2e39d80296415e44af7bb
刘园园 <liuyuanyuan@tungee.com>
2020-07-30 00:37:09 +0800

    feat: hello world

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/07/30 00:35:30
+@Author  :   Liu YuanYuan :)
+@Version :   1.0
+@Desc    :   None
+'''
+
+# here put the import lib
+
+
+if __name__ == "__main__":
+    print("hello spider!")
